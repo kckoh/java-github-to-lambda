3314487c3c16d6d7246b26530fba922ef6163291
kckoh <kyungcheolkoh@gmail.com>
2024-05-24 19:39:58 -0700

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+// package com.example;
+
+// import com.amazonaws.services.lambda.runtime.Context;
+// import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+// public class Main implements RequestHandler<String, String> {
+//     @Override
+//     public String handleRequest(String input, Context context) {
+//         return "Hello, " + input + "!";
+//     }
+
+// }
+
+package src.main.java.com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

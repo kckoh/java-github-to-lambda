425dbbeac295d9c92a8d52c04fd1733edb5d4e74
kckoh <kyungcheolkoh@gmail.com>
2024-06-06 20:41:47 -0700

    changed something

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,11 +2,12 @@ package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.Map;
 
-public class App implements RequestHandler<String, String> {
-    @Override
-    public String handleRequest(String input, Context context) {
-        return "Hello, " + input + "!";
+public class App implements RequestHandler<Map<String, Object>, String> {
+    public String handleRequest(Map<String, Object> input, Context context) {
+        System.out.println("Hello World! executed with input: " + input);
+        return input.toString();
     }
 
     public static void main(String[] args) {

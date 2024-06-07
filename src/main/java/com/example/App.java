76bb6bd7dde91f8f5720ec831113d9fe973f2555
kckoh <kyungcheolkoh@gmail.com>
2024-06-06 19:49:51 -0700

    hanlde

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,20 +1,22 @@
-// package com.example;
+package com.example;
 
-// import com.amazonaws.services.lambda.runtime.Context;
-// import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-// public class Main implements RequestHandler<String, String> {
-//     @Override
-//     public String handleRequest(String input, Context context) {
-//         return "Hello, " + input + "!";
-//     }
+public class App implements RequestHandler<String, String> {
+    @Override
+    public String handleRequest(String input, Context context) {
+        return "Hello, " + input + "!";
+    }
 
-// }
-
-package src.main.java.com.example;
-
-public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
 }
+
+// public class App {
+// public static void main(String[] args) {
+// System.out.println("Hello, World!");
+// }
+// }
